7d9f0767ab699fa2204b6f219a66789e1429c898
ryantroywilson <ryantroywilson@gmail.com>
2018-01-17 09:31:03 -0800

    Add files via upload

---
+++ multiples_sums_average.py
@@ -0,0 +1,18 @@
+#multiples
+for count in range (1,1001,2):
+    print count
+
+for count in range(5,1000000,5):
+    print count
+
+#sums
+my_numbers = [1,2,3,4,5,10]
+sum = 0 
+for i in my_numbers:
+    sum += i 
+print sum 
+
+#average list
+
+print sum/len(my_numbers)
+
